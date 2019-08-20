344dd85338426ba3b790447a33474ed1f32d3622
admin <admin@admins-MacBook-Pro.local>
2019-08-20 11:31:05 -0700

    closure finished

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -4,31 +4,31 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 /* 
 
-  // GIVEN THIS PROBLEM:
+  GIVEN THIS PROBLEM:
 
   function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
+    firstItem passes the first item of the given array to the callback function.
   }
 
-  // SOLUTION:
+  SOLUTION:
 
   function firstItem(arr, cb) {
     return cb(arr[0]);
   }
 
-  // NOTES ON THE SOLUTION:
+  NOTES ON THE SOLUTION:
 
-  // firstItem is a higher order function.
-  // It expects a callback (referred to as `cb`) as its second argument.
-  // To test our solution, we can use the given `items` array and a variety of callbacks.
-  // Note how callbacks can be declared separately, or inlined.
+  firstItem is a higher order function.
+  It expects a callback (referred to as `cb`) as its second argument.
+  To test our solution, we can use the given `items` array and a variety of callbacks.
+  Note how callbacks can be declared separately, or inlined.
 
-  // TEST 1 (inlined callback):
+  TEST 1 (inlined callback):
 
   const test1 = firstItem(items, item => `I love my ${item}!`);
   console.log(test1); // "I love my Pencil!"
 
-  // TEST 2 (declaring callback before hand):
+  TEST 2 (declaring callback before hand):
 
   function logExorbitantPrice(article) {
     return `this ${article} is worth a million dollars!`;
